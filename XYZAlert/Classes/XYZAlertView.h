9bfe88c8c48c9bcf81e111228a88ca895fca5dab
大大东 <lidong@021.com>
2023-02-20 17:36:11 +0800

    feat: property support APPEARANCE set

--- XYZAlert/Classes/XYZAlertView.h
+++ XYZAlert/Classes/XYZAlertView.h
@@ -16,23 +16,24 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, strong) UIView *containerAlertView;
 
 /// 容器Alert最大size
-/// defult screenWitdh==320 ? CGSizeMake(280, 400) : CGSizeMake(310, 500);
-@property (nonatomic, assign) CGSize containerAlertViewMaxSize;
+/// defult screenWitdh == 320 ? CGSizeMake(280, 400) : CGSizeMake(310, 500);
+@property (nonatomic, assign) CGSize containerAlertViewMaxSize UI_APPEARANCE_SELECTOR;
 
 /// 容器Alert圆角半径
-@property (nonatomic, assign) CGFloat containerAlertViewRoundValue;
+/// /// defult 10
+@property (nonatomic, assign) CGFloat containerAlertViewRoundValue UI_APPEARANCE_SELECTOR;
 
 /// 背景透明度
 /// defult 0.3
-@property (nonatomic, assign) CGFloat backAlpha;
+@property (nonatomic, assign) CGFloat backAlpha UI_APPEARANCE_SELECTOR;
 
 /// 点击空白区域时自动隐藏
 /// defult YES
-@property (nonatomic, assign) BOOL hideOnTouchOutside;
+@property (nonatomic, assign) BOOL hideOnTouchOutside UI_APPEARANCE_SELECTOR;
 
 /// 自动躲避键盘 (键盘弹出时 自动往上便宜键盘的高度)
 /// defult YES
-@property (nonatomic, assign) BOOL autoAvoidKeyboard;
+@property (nonatomic, assign) BOOL autoAvoidKeyboard UI_APPEARANCE_SELECTOR;
 
 /// 自动躲避键盘时修正偏移量
 /// 键盘弹出时 默认自动往上偏移键盘的高度 此值<0时 会增加向上偏移的距离, 此值>0时 会减小向上偏移的距离
